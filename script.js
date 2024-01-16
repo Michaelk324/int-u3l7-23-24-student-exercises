8db57e1615380d4057873d1a79d4bc62e9a4730e
Michaelk324 <147104243+Michaelk324@users.noreply.github.com>
2024-01-16 22:24:16 +0000

    Review

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // 1. Declare a variable called myName. 
+let myName = "Michael";
 //  - Give it the value of your name. 
 //  - This is a string so the value should be in quotes ""
 
 
 
 // 2. Declare a variable called age. 
+let age = 16;
 //  - Give it the value of your age. 
 //  - This is a number, so the value does not have to be in quotes
 
 // 3. Declare a variable called answer.
+let answer = document.querySelector(".answer");
+
 //  - This should save the div with a class of `answer`
 
 
 // 4. When you're done, remove the comment from the code below.
-
-// answer.innerHTML = myName + " is " + age;
+answer.innerHTML = myName + " is " + age;
